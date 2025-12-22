becd3ec4cd984f566320ddaeeab50d3748369234
Grécio Diogo <fonebahia8@gmail.com>
2025-12-22 15:30:05 +0100

    feat: remove role

--- app/Modules/Authentication/routes/users.routes.js
+++ app/Modules/Authentication/routes/users.routes.js
@@ -1,11 +1,11 @@
-
+E
     module.exports = (ApiRoute, Route) => 
     // Protected routes
     ApiRoute(() => {
-      Route.get("/", "UsersController.index").middleware(['role:admin']);
-      Route.post("/", "UsersController.store").middleware(['role:admin']);
-      Route.get("/:id", "UsersController.show").middleware(['role:admin']);
-      Route.put("/:id", "UsersController.update").middleware(['role:admin']);
-      Route.delete("/:id", "UsersController.destroy").middleware(['role:admin']);
+      Route.get("/", "UsersController.index");
+      Route.post("/", "UsersController.store");
+      Route.get("/:id", "UsersController.show");
+      Route.put("/:id", "UsersController.update");
+      Route.delete("/:id", "UsersController.destroy");
     }, 'users').namespace("App/Modules/Authentication/Controllers").middleware(["auth"]);
     
